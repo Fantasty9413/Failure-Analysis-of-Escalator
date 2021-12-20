1117e5d8045e99a46f37896e41e380b24c09212d
Fantasty9413 <969197213@qq.com>
2021-12-20 11:11:05 +0800

    update to lib version

--- TypicalValue/TV_kv.m
+++ TypicalValue/TV_kv.m
@@ -1,12 +1,12 @@
-function [ kv ] = TV_kv( TD )
+function [ kv ] = TV_kv( TD_amp )
 %   TV_kv: get kurtosis value(kv) of typical value(TV)
 %   Inputs:
-%       TD: data struct of time domain(TD) signal
+%       TD_amp: amplitude of data struct of time domain(TD) signal
 %   Outputs:
 %       kv: kurtosis value
 
-avg = mean(TD.data(:));
-var = mean((TD.data-avg).^2);
+avg = mean(TD_amp(:));
+var = mean((TD_amp-avg).^2);
 kv = avg^4 / var^2;
 
 end
--- TypicalValue/TV_pvifds.m
+++ TypicalValue/TV_pvifds.m
@@ -1,25 +1,26 @@
-function [ pv, f ] = TV_pvifds( FD, fds )
+function [ pvifds_amp, pvifds_f ] = TV_pvifds( FD_amp, FD_f )
 %   TV_pvifds: pv in fd segment, get pv of acceleration in low, mid or high
 %   frequency segment
 %   Inputs:
-%       FD:  data struct of frequency domain(FD) signal
-%       fds: option of chose low, mid or high segment
+%       FD_amp: amplitude of FD data
+%       FD_f:   frequency of FD data
 %   Outputs:
-%       pv:  pv value of acceleration in specified segment
-%       f:   frequency corresponding of pv
+%       pvifds_amp:  pv value of acceleration in low, mid and high segment
+%       pvifds_f:    frequency corresponding of pv
 
-keyword = {'low', 'mid', 'high'};       % option of fds
-
-index = find(strcmp(keyword,fds) == 1); % index of fds in keyword
-
-L = length(FD.data);        % length of FD.data
+L = length(FD_amp);        % length of FD.data
 L_segment = floor(L/3);     % length of segment(low frequency, mid frequency and high frequency)
 
-s_start = (index-1)*L_segment + 1;  % the start index of this segment
-s_end = index*L_segment;            % the end index of this segment
+pvifds_amp = zeros(1,3);
+pvifds_f = zeros(1,3);
 
-pv = max(FD.data(s_start:s_end));
-f = FD.f(find(FD.data(s_start:s_end) == pv));
+for index = 1:1:3
+    s_start = (index-1)*L_segment + 1;  % the start index of this segment
+    s_end = index*L_segment;            % the end index of this segment
+
+    pvifds_amp(index) = max(FD_amp(s_start:s_end));
+    pvifds_f(index) = FD_f(find(FD_amp(s_start:s_end) == pvifds_amp(index)) + s_start - 1);
+end
 
 end
 
